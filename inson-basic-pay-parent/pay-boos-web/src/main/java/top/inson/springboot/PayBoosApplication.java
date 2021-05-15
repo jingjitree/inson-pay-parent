06a86d275cc78cd0d54ba431694bc881c6c9c230
jingjitree <jingjitree@gmai.com>
2021-05-15 11:54:11 +0800

    '后台管理模块'

---
+++ inson-basic-pay-parent/pay-boos-web/src/main/java/top/inson/springboot/PayBoosApplication.java
@@ -0,0 +1,14 @@
+package top.inson.springboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayBoosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayBoosApplication.class, args);
+    }
+
+}
