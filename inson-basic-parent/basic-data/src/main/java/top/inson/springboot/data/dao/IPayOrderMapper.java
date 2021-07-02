aa6b07fd548b49c74e600c12c7db836ee98441ec
jingjitree <jingjitree@gmai.com>
2021-07-02 11:19:30 +0800

    '数据dao查询'

---
+++ inson-basic-parent/basic-data/src/main/java/top/inson/springboot/data/dao/IPayOrderMapper.java
@@ -0,0 +1,8 @@
+package top.inson.springboot.data.dao;
+
+import top.inson.springboot.data.base.ITKBaseMapper;
+import top.inson.springboot.data.entity.PayOrder;
+
+public interface IPayOrderMapper extends ITKBaseMapper<PayOrder> {
+
+}
