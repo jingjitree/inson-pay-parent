b9e555e70e41246d37278c6a0eba8705ee66f078
jingjitree <jingjitree@gmai.com>
2021-07-05 16:31:03 +0800

    '支付接口aop拦截签名'

--- inson-basic-pay-parent/pay-core-web/src/main/java/top/inson/springboot/pay/core/PayApiAspect.java
+++ inson-basic-pay-parent/pay-core-web/src/main/java/top/inson/springboot/pay/core/PayApiAspect.java
@@ -8,11 +8,15 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 
 @Slf4j
 @Aspect
@@ -31,11 +35,39 @@ public class PayApiAspect {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
         String signType = request.getHeader("signType");
         String paySign = request.getHeader("paySign");
+        MethodSignature signature = (MethodSignature)joinPoint.getSignature();
+        Method method = signature.getMethod();
 
+        //方法的所有参数
+        Object[] params = joinPoint.getArgs();
+        if (params.length == 0){
+            return joinPoint.proceed();
+        }
 
+        //方法参数的注解
+        Annotation[][] annotations = method.getParameterAnnotations();
+        for (int i = 0, len = annotations.length; i < len; i++) {
+            Object param = params[i];
+            Annotation[] annotation = annotations[i];
+            if (param == null || annotation.length == 0)
+                continue;
+            for (Annotation an : annotation) {
+                if (an.annotationType().equals(RequestBody.class)){
+                    String reqJson = gson.toJson(param);
+                    log.info("{},方法的请求参数json:{}", method.getName(), reqJson);
+                    this.validateSign(signType, paySign, reqJson);
+                }
+            }
+        }
         Object result = joinPoint.proceed();
         log.info("接口应答参数：{}", gson.toJson(result));
         return result;
+    }
+
+    private void validateSign(String signType, String paySign, String reqJson) {
+        log.info("signType:{}", signType);
+        log.info("paySign:{}", paySign);
+        //throw new BadRequestException("签名失败");
     }
 
 }
