5232a0e4866d2c67a5bc44a64e0a2368b4a57fb7
jingjitree <jingjitree@gmai.com>
2021-07-06 16:15:04 +0800

    '策略模式service处理'

--- inson-basic-pay-parent/pay-core-web/src/main/java/top/inson/springboot/pay/strategy/IStrategyService.java
+++ inson-basic-pay-parent/pay-core-web/src/main/java/top/inson/springboot/pay/strategy/IStrategyService.java
@@ -2,6 +2,9 @@ package top.inson.springboot.pay.strategy;
 
 import top.inson.springboot.pay.service.channel.IChannelService;
 
+/**
+ * 策略模式的service
+ */
 public interface IStrategyService {
 
     IChannelService getChannelService(String source);
