8ec89f959474f2d068601744670f3262ec95bf87
jingjitree <jingjitree@gmai.com>
2021-07-06 16:12:25 +0800

    '支付常量'

---
+++ inson-basic-pay-parent/pay-core-web/src/main/java/top/inson/springboot/pay/constant/PayRedisConstant.java
@@ -0,0 +1,15 @@
+package top.inson.springboot.pay.constant;
+
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class PayRedisConstant {
+
+    public static final String PAY_CASHIER_PREFIX = "payCashier:";
+
+    public static final String CASHIER_KEY = "cashier";
+
+
+}
