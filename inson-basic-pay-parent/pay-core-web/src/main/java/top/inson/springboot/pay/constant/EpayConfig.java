2cadc2cc5e5dbb13f53afe2be7033cf52a43cf62
jingjitree <jingjitree@gmai.com>
2021-07-09 18:18:47 +0800

    'epay常量配置'

---
+++ inson-basic-pay-parent/pay-core-web/src/main/java/top/inson/springboot/pay/constant/EpayConfig.java
@@ -0,0 +1,29 @@
+package top.inson.springboot.pay.constant;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "epay")
+@PropertySource(value = "config/epay/epayConfig.properties")
+public class EpayConfig {
+
+    private String certNo;
+
+    private String certPwd;
+
+    private String certPath;
+
+    private String baseUrl;
+
+    private String unifiedUrl;
+
+
+}
