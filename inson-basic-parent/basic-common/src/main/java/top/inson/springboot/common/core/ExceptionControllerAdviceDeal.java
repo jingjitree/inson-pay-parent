c4da6201c6a353ef036095e9a5872fbb0248169c
jingjitree <jingjitree@gmai.com>
2021-07-09 18:17:34 +0800

    '全局异常处理'

--- inson-basic-parent/basic-common/src/main/java/top/inson/springboot/common/core/ExceptionControllerAdviceDeal.java
+++ inson-basic-parent/basic-common/src/main/java/top/inson/springboot/common/core/ExceptionControllerAdviceDeal.java
@@ -4,6 +4,7 @@ package top.inson.springboot.common.core;
 import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -31,12 +32,17 @@ public class ExceptionControllerAdviceDeal {
     @ExceptionHandler(value = MissingServletRequestParameterException.class)
     public CommonResult missingServletRequestParamHandler(Exception e){
         log.info("缺少参数异常", e);
-        return CommonResult.fail(502, "缺少必填参数");
+        return CommonResult.fail(HttpStatus.BAD_GATEWAY.value(), "缺少必填参数");
     }
 
     @ExceptionHandler(value = BadRequestException.class)
     public CommonResult badRequestHandler(Exception e){
         return CommonResult.fail(HttpStatus.BAD_REQUEST.value(), StrUtil.isEmpty(e.getMessage()) ? "参数有误" : e.getMessage());
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public CommonResult argsValidHandler(Exception e){
+        return CommonResult.fail(HttpStatus.BAD_GATEWAY.value(), StrUtil.isEmpty(e.getMessage()) ? "参数校验失败" : e.getMessage());
     }
 
 }
