128e7d7d6c508e054de9ae8494f1e30cdea88ca1
jingjitree <jingjitree@gmai.com>
2021-07-24 15:57:53 +0800

    '队列参数常量类'

---
+++ inson-basic-parent/basic-pay-common/src/main/java/top/inson/springboot/paycommon/constant/PayMqConstant.java
@@ -0,0 +1,14 @@
+package top.inson.springboot.paycommon.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class PayMqConstant {
+
+    public static final String NOTIFY_URL = "notifyUrl";
+    public static final String NOTIFY_DATA = "data";
+    public static final String NOTIFY_HEADERS = "headers";
+    public static final String NOTIFY_COUNT = "notifyCount";
+
+}
