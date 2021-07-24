198019deea344cc0fb8ebe711ee3a3bb5927af18
jingjitree <jingjitree@gmai.com>
2021-07-24 13:54:42 +0800

    '退款回调请求实体'

---
+++ inson-basic-parent/basic-pay-common/src/main/java/top/inson/springboot/paycommon/entity/dto/RefundNotifyDto.java
@@ -0,0 +1,15 @@
+package top.inson.springboot.paycommon.entity.dto;
+
+
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ApiModel(value = "退款回调实体")
+public class RefundNotifyDto extends RefundBaseDto implements java.io.Serializable{
+
+
+
+}
