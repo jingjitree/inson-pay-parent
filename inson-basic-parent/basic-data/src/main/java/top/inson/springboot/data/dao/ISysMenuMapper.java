b7cc5ffc2f5096d76cf163f2387640665fbf9a97
jingjitree <jingjitree@gmai.com>
2021-07-30 17:03:38 +0800

    '数据实体'

--- inson-basic-parent/basic-data/src/main/java/top/inson/springboot/data/dao/ISysMenuMapper.java
+++ inson-basic-parent/basic-data/src/main/java/top/inson/springboot/data/dao/ISysMenuMapper.java
@@ -3,7 +3,11 @@ package top.inson.springboot.data.dao;
 import top.inson.springboot.data.base.ITKBaseMapper;
 import top.inson.springboot.data.entity.SysMenu;
 
+import java.util.List;
+
 public interface ISysMenuMapper extends ITKBaseMapper<SysMenu> {
+
+    List<SysMenu> selectUserRoleMenu(Integer usersId);
 
 
 }
