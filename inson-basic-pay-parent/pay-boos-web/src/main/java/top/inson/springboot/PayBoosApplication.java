bb48a992c43989059b3d2605ad03b99d51b3fc4f
jingjitree <jingjitree@gmail.com>
2021-08-02 18:17:22 +0800

    '开启cache'

--- inson-basic-pay-parent/pay-boos-web/src/main/java/top/inson/springboot/PayBoosApplication.java
+++ inson-basic-pay-parent/pay-boos-web/src/main/java/top/inson/springboot/PayBoosApplication.java
@@ -4,9 +4,12 @@ package top.inson.springboot;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableSwagger2Doc
+//开启缓存
+@EnableCaching
 public class PayBoosApplication {
 
     public static void main(String[] args) {
